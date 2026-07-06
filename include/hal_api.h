b6af9873105c87f2a1572c56f098c71782d5da49
lakshmikanth <ssnlakshmikanth@gmail.com>
2026-07-06 10:24:43 +0530

    initial commit

---
+++ include/hal_api.h
@@ -0,0 +1,5 @@
+#ifndef HAL_API_H
+#define HAL_API_H
+#define HAL_API_VERSION 1
+void dma_start(void);
+#endif
---
+++ src/hal_api.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#include "hal_api.h"
+void dma_start(void){printf("HAL DMA\n");}
