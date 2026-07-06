b4a48d00f84ca5caca3e2cd0b65e34408e70c385
lakshmikanth <ssnlakshmikanth@gmail.com>
2026-07-06 11:00:30 +0530

    test API working

--- include/hal_api.h
+++ include/hal_api.h
@@ -2,4 +2,5 @@
 #define HAL_API_H
 #define HAL_API_VERSION 1
 void dma_start(void);
+const char* hal_version(void);
 #endif
--- src/hal_api.c
+++ src/hal_api.c
@@ -1,3 +1,4 @@
 #include <stdio.h>
 #include "hal_api.h"
-void dma_start(void){printf("HAL DMA\n");}
+void dma_start(void){printf("[HAL] DMA Started\n");}
+const char* hal_version(void){return "HAL v1.0";}
